dc7bd828c47bfc39b90dd595f3341cf9b3ffdd0a
SonNguyenTrong <trrongson1999@gmail.com>
2018-11-19 20:42:24 +0700

    mimodel

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        $number = 10;
+
+        for ($i = 0; $i < $number; $i++) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->email,
+            ]);
+        }
+    }
+}
