7b7e3e82c5a1c21eae4174f18ec9cb97687c3269
doanhvv <doanhdev@gmail.com>
2018-11-21 20:41:38 +0700

    make auth

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Ticket::class, function (Faker $faker) {
+
+    $title = $faker->sentence($nbWords = 6, $variableNbWords = true);
+
+    return [
+        'title' => $title,
+        'content' => $faker->text,
+        'slug' => str_slug($title),
+        'status' => rand(0,1),
+        'user_id' => rand(1, 3)
+    ];
+});
