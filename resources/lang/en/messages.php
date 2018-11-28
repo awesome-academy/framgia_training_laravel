b734e432f16fe758ed188f4d3939615f8e1af72d
SonNguyenTrong <trrongson1999@gmail.com>
2018-11-28 21:08:48 +0700

    cruduser

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,5 +3,10 @@
 return [
 
     'home' => 'Main Page',
+    'list' => 'List User',
+    'create' => 'Create',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'confirm' => 'Are You Sure?',
 
 ];
