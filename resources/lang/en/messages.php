f43498319de8a83570f2e7441fa28de857d4c378
SonNguyenTrong <trrongson1999@gmail.com>
2018-11-30 18:40:52 +0700

    usercrud

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,5 +3,14 @@
 return [
 
     'home' => 'Main Page',
+    'list' => 'List User',
+    'create' => 'Create',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'confirm' => 'Are You Sure?',
+    'user' => 'Username',
+    'email' => 'Email',
+    'action' => 'Action',
+    'submit' => 'Submit',
 
 ];
