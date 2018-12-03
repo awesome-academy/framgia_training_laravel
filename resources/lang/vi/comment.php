1747b725ccf48148e80876cdfc13bf6c34756751
Doanh <doanhdev@gmail.com>
2018-12-03 16:42:51 +0700

    comment

--- resources/lang/vi/comment.php
+++ resources/lang/vi/comment.php
@@ -4,18 +4,18 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Action Language Lines
+    | Comemnt Language Lines
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used during action for various
+    | The following language lines are used during comemnt for various
     | messages that we need to display to the user. You are free to modify
     | these language lines according to your application's requirements.
     |
     */
 
-    'show' => 'Xem',
-    'edit' => 'Sửa',
-    'delete' => 'Xóa',
-    'question' => 'Bạn có chắc bạn muốn xóa mục này ?',
+    'comment' => 'Nhận xét',
+    'reply' => 'Trả lời',
+    'save' => 'Đắng nhận xét',
+    'nocomment' => 'Chưa có comment nào, hãy viết một cái gì đó.',
 
 ];
